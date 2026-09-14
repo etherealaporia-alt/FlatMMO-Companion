1873a8a6464bfafc2b0f30e55f461d3c2079e922
BjornThorson <etherealaporia@gmail.com>
2026-09-14 20:07:42 +0000

    Add current FlatMMO companion prototype

---
+++ database.js
@@ -0,0 +1,38 @@
+window.rawMonsterLogs = {
+
+    gorilla: `[
+        "gorilla",
+        "HP: 70",
+        "Accuracy: 12",
+        "Max Hit: 10",
+        "Defence: 10",
+        "Magic Defence: 0",
+        "Weakness: none",
+        "POSSIBLE DROPS",
+        "Name\t\tAmount\tChance",
+        "giant_bones\t\t1\t1/1",
+        "banana\t\t1\t1/2",
+        "dotted_green_leaf\t\t1\t1/6",
+        "green_leaf\t\t1\t1/10",
+        "lime_leaf\t\t1\t1/20",
+        "gold_leaf\t\t1\t1/30",
+        "gorilla_foot_artifact\t\t1\t1/100"
+    ]`, // 🌟 Added a comma here to neatly separate the monsters!
+
+    chicken: `[
+        "chicken",
+        "HP: 3",
+        "Accuracy: 0",
+        "Max Hit: 0",
+        "Defence: 0",
+        "Magic Defence: 0",
+        "Weakness: none",
+        "POSSIBLE DROPS",
+        "Name\t\tAmount\tChance",
+        "bones\t\t1\t1/1",
+        "raw_chicken\t\t1\t1/1",
+        "feathers\t\t2 - 6\t1/1",
+        "wheat_seeds\t\t1\t1/3"
+    ]` // 🌟 Both monsters are now tucked safely inside the main closing bracket below.
+
+};
